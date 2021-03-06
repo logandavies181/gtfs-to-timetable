d5660adf2e8916815c6c1abb3c1235b3a813a79e
logandavies181 <logan.davies181@gmail.com>
2021-03-06 04:04:22 +0000

    add order info

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 //use std::fs::write;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::time::Instant;
 
 use gtfs_structures::*;
 use serde::{Deserialize, Serialize};
+use topological_sort::*;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
@@ -12,6 +13,7 @@ struct Dat {
     sid_to_dates: HashMap<String, Vec<String>>,
     date_to_sids: HashMap<String, Vec<String>>,
     sid_to_trips: HashMap<String, Vec<String>>,
+    route_orders: HashMap<String, Vec<String>>,
 }
 
 impl Dat {
@@ -21,9 +23,11 @@ impl Dat {
             sid_to_dates: HashMap::new(),
             date_to_sids: HashMap::new(),
             sid_to_trips: HashMap::new(),
+            route_orders: HashMap::new(),
         };
         dat.order_sid_to_dates();
         dat.order_sid_to_trips();
+        dat.get_route_orders();
         dat
     }
 }
@@ -62,9 +66,89 @@ impl Dat {
             push_value_to_hashmap_vec(&mut self.sid_to_trips, trip.service_id.clone(), trip.id.clone())
         }
     }
+    fn get_route_orders(&mut self) {
+        let mut route_id_to_trips: HashMap<String, Vec<&Trip>> = HashMap::new();
+        let mut route_id_to_inbound_only_trips: HashMap<String, Vec<&Trip>> = HashMap::new();
+        let mut inbound_only_routes = HashSet::new();
+        for trip in self.g.trips.values() {
+            match trip.direction_id {
+                Some(x) => {
+                    match x {
+                        DirectionType::Outbound => push_value_to_hashmap_vec(&mut route_id_to_trips, trip.route_id.clone(), trip),
+                        DirectionType::Inbound => {},
+                    }
+                },
+                _ => panic!("could not sort stops due to not knowing direction of travel"),
+            }
+        }
+
+        // Get trips for routes that are inbound only
+        for route_id in self.g.routes.keys() {
+            if !route_id_to_trips.contains_key(route_id.as_str()) {
+                inbound_only_routes.insert(route_id.clone());
+            }
+        }
+        for trip in self.g.trips.values() {
+            if inbound_only_routes.contains(&trip.route_id) {
+                match trip.direction_id {
+                    Some(x) => {
+                        match x {
+                            DirectionType::Outbound => panic!(format!("thought that route {} was inbound only but found outbound route", &trip.route_id)),
+                            DirectionType::Inbound => {
+                                push_value_to_hashmap_vec(&mut route_id_to_inbound_only_trips, trip.route_id.clone(), trip);
+                            },
+                        }
+                    },
+                    _ => panic!("Somehow got a direction before but not now"),
+                }
+            }
+        }
+
+        for (route_id, trips) in route_id_to_trips.iter() {
+            let order = get_outbound_trip_order(trips.to_vec(), route_id.clone());
+            self.route_orders.insert(route_id.clone(), order);
+        }
+
+        for (route_id, trips) in route_id_to_inbound_only_trips.iter() {
+            println!("trying inbound for {}", route_id);
+            let mut order = get_outbound_trip_order(trips.to_vec(), route_id.clone());
+            order.reverse();
+            self.route_orders.insert(route_id.clone(), order);
+        }
+    }
 }
 
-fn push_value_to_hashmap_vec(hash: &mut HashMap<String, Vec<String>>, key: String, value: String) {
+fn get_outbound_trip_order(trips: Vec<&Trip>, route_id: String) -> Vec<String> {
+        //let mut dependencies = HashSet::new();
+        let mut topologicalsort: TopologicalSort<String> = TopologicalSort::new();
+        for trip in trips.iter() {
+            let mut prev: &StopTime = &trip.stop_times[0];
+            for count in 0..trip.stop_times.len()-1 {
+                let stime = &trip.stop_times[count];
+                if count == 0 {
+                    prev = stime;
+                    continue;
+                }
+                topologicalsort.add_dependency(stime.stop.id.clone(), prev.stop.id.clone());
+                prev = stime;
+            }
+        }
+        let mut order: Vec<String> = Vec::new();
+        while topologicalsort.len() != 0 {
+            let next_opt = topologicalsort.pop();
+            match next_opt {
+                Some(x) => order.push(x),
+                None => {
+                    println!("could not sort route {}", route_id);
+                    return order
+                },
+            }
+        }
+
+        order
+}
+
+fn push_value_to_hashmap_vec<T>(hash: &mut HashMap<String, Vec<T>>, key: String, value: T) {
     let vec_opt = hash.get_mut(key.as_str());
     match vec_opt {
         Some(v) => v.push(value),
@@ -92,6 +176,8 @@ struct RouteInfo {
     route_id: String,
     inbound: Vec<TripInfo>,
     outbound: Vec<TripInfo>,
+    inbound_order: Vec<String>,
+    outbound_order: Vec<String>,
 }
 
 impl RouteInfo {
@@ -100,6 +186,8 @@ impl RouteInfo {
             route_id: String::from(""),
             inbound: Vec::new(),
             outbound: Vec::new(),
+            inbound_order: Vec::new(),
+            outbound_order: Vec::new(),
         }
     }
 }
@@ -107,55 +195,16 @@ impl RouteInfo {
 #[derive(Serialize, Deserialize)]
 struct TripInfo {
     times: HashMap<String, String>,
+    first_time: u32,
 }
 
 impl TripInfo {
     fn new() -> TripInfo {
         TripInfo {
             times: HashMap::new(),
+            first_time: std::u32::MAX,
         }
     }
-}
-
-fn main() -> Result<()> {
-    println!("Parsing gtfs data...");
-    let start = Instant::now();
-    let gtfs = Gtfs::from_path("./files/")?;
-    println!("Parsed gtfs files in: {}s", start.elapsed().as_secs());
-
-    let parse_dat_time = Instant::now();
-    let dat = Dat::from_gtfs(gtfs);
-    println!("Parsed gtfs data in: {}s", parse_dat_time.elapsed().as_secs());
-
-    /*
-    for tripid in dat.sid_to_trips.get("Rail MTuWThF-XHol").expect("failed2").iter(){
-        let trip = dat.g.trips.get(tripid.as_str()).expect("failed3");
-        add_trip_to_route_info(&mut route_id_to_route_info, trip);
-    }
-    */
-    for (date, sids) in dat.date_to_sids {
-        let mut route_id_to_route_info: HashMap<String, RouteInfo> = HashMap::new();
-        for sid in sids {
-            for trip_id in dat.sid_to_trips.get(sid.as_str()).expect("failed2").iter() {
-                let trip = dat.g.trips.get(trip_id.as_str()).expect("failed3");
-                add_trip_to_route_info(&mut route_id_to_route_info, trip);
-            }
-        }
-
-        for (key, value) in route_id_to_route_info.iter() {
-            let data = serde_json::to_string(&value)?;
-            std::fs::create_dir_all(format!("output/{}", date))?;
-            std::fs::write(format!("output/{}/{}.json", date, key), data)?;
-        }
-    }
-
-    /*
-    let j = serde_json::to_string(&route_id_to_route_info)?;
-
-    println!("{}", j);
-    */
-
-    Ok(())
 }
 
 // Gets all of the stop times and direction from a trip and adds it to the corresponding
@@ -163,9 +212,16 @@ fn main() -> Result<()> {
 fn add_trip_to_route_info(riri: &mut HashMap<String, RouteInfo>, trip: &Trip) {
     let mut trip_info = TripInfo::new();
 
+    let mut first_time: u32 = std::u32::MAX;
+    // FIXME: consider departure_time also
     for stime in trip.stop_times.iter() {
-        trip_info.times.insert(parse_time(stime.arrival_time.expect("failed4")), stime.stop.id.clone());
+        let arrival_time = stime.arrival_time.expect("No arrival time");
+        if arrival_time < first_time {
+            first_time = arrival_time;
+        }
+        trip_info.times.insert(stime.stop.id.clone(), parse_time(arrival_time));
     }
+    trip_info.first_time = first_time;
 
     let route_id = trip.route_id.clone();
     let route_info = riri.entry(route_id.clone());
@@ -197,6 +253,45 @@ fn add_trip_to_route_info(riri: &mut HashMap<String, RouteInfo>, trip: &Trip) {
             };
         }
     }
+}
+
+fn main() -> Result<()> {
+    println!("Parsing gtfs data...");
+    let start = Instant::now();
+    let gtfs = Gtfs::from_path("./files/")?;
+    println!("Parsed gtfs files in: {}s", start.elapsed().as_secs());
+
+    let parse_dat_time = Instant::now();
+    let dat = Dat::from_gtfs(gtfs);
+    println!("Parsed gtfs data in: {}s", parse_dat_time.elapsed().as_secs());
+
+    // Loop over each date and then get all services that run on that day to get all the trips
+    // that run on that day
+    for (date, sids) in dat.date_to_sids {
+        let mut route_id_to_route_info: HashMap<String, RouteInfo> = HashMap::new();
+        for sid in sids {
+            for trip_id in dat.sid_to_trips.get(sid.as_str()).expect("failed2").iter() {
+                let trip = dat.g.trips.get(trip_id.as_str()).expect("failed3");
+                add_trip_to_route_info(&mut route_id_to_route_info, trip);
+            }
+        }
+
+        // Print all of the trips for a route on a given day to a file
+        for (key, value) in route_id_to_route_info.iter_mut() {
+            let stop_order_opt = dat.route_orders.get(key.clone().as_str());
+            match stop_order_opt {
+                Some(x) => value.stop_order = x.to_vec(),
+                None => {
+                    //println!("{:?}", dat.route_orders);
+                },
+            }
+            let data = serde_json::to_string(&value)?;
+            std::fs::create_dir_all(format!("output/{}", date))?;
+            std::fs::write(format!("output/{}/{}.json", date, key), data)?;
+        }
+    }
+
+    Ok(())
 }
 
 /* TODO:
